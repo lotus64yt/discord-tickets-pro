08588df7a3e3bb4817ca62683c01db196b8bc056
lotus64 <114228798+lotus64yt@users.noreply.github.com>
2024-05-27 22:18:23 +0200

    Update exemple.js

--- exemple.js
+++ exemple.js
@@ -1,5 +1,6 @@
 const { GatewayIntentBits } = require("discord.js")
 const Discord = require("discord.js")
+const { TicketPanelEmbed, TicketsManager } = require("./index")
 
 const Client = new Discord.Client({
     intents: GatewayIntentBits.Guilds
@@ -20,26 +21,101 @@ const Client = new Discord.Client({
         | GatewayIntentBits.MessageContent
 });
 
-Client.login("your-token-here")
+Client.login("Your token here")
 
-const Tickets = require("./index")
-const { ButtonStyle } = require("discord.js"); // Add this line
+// You can access the TicketsManager in the client if you do this :
+// Client.TicketsManager = new TicketsManager(Client)
 
-Client.on("ready", client => {
-    const ticket = new Tickets(Client)
-        .setGuild("1096520545656393739")
-        .setChannel("1096520546306490390")
-        .setButtonLabel("Open Ticket")
-        .setButtonStyle(ButtonStyle.Primary)
-        .setButtonEmoji("🎫")
-        .setMessage("This is the message")
-        .setTitle("This is a title")
-        .setThumbnail("https://images-ext-1.discordapp.net/external/kpnOMiwK5EiAAtlvLDEQ7k3YAdIfiytCQA7PlVJkoJI/https/cdn.discordapp.com/avatars/922130278443528232/a_86e367b840be27fafa58564086ac4a9d.gif?width=192&height=192")
-        .setFooter("discord-tickets-pro")
-        .setCreatedTicketMessage("Ticket created by {member}")
-        .setStaffRole("1173039883707764797")
+Client.on("ready", () => console.log("Ready"))
 
-    ticket.send()
-        .then(() => console.log("Ticket sent"))
-        .catch(console.error)
+Client.on("interactionCreate", async interaction => {
+    if (interaction.customId === "ticketOpen") {
+        await interaction.guild.members.fetch() // If you map the members who have a role
+
+        const members = interaction.guild.roles.cache.get("1173039883707764797").members.map(e => e.user)
+
+        new TicketsManager(Client)
+            .createTicket({
+                guild: "guildId",
+                interaction: interaction,
+                components: [
+                    new Discord.ActionRowBuilder()
+                        .setComponents([
+                            new Discord.ButtonBuilder()
+                                .setEmoji("🚮")
+                                .setStyle(Discord.ButtonStyle.Danger)
+                                .setCustomId("ticketDelete"),
+                            new Discord.ButtonBuilder()
+                                .setCustomId("ticketArchive")
+                                .setEmoji("⛔")
+                                .setStyle(Discord.ButtonStyle.Primary),
+                            new Discord.ButtonBuilder()
+                                .setCustomId("ticketTranscript")
+                                .setEmoji("🪪")
+                                .setStyle(Discord.ButtonStyle.Secondary)
+                        ])
+                ],
+                accessMembers: members, //user objects (Array)
+                embeds: [
+                    new Discord.EmbedBuilder()
+                        .setTitle("Discord-tickets-pro")
+                        .setDescription(`<@${interaction.user.id}> have open a ticket.`)
+                ],
+                //  parentId: "1222274711182901349" // if null, the ticket will be in a thread
+            })
+    } else if (interaction.customId === "ticketArchive") { // dont work on threads
+        new TicketsManager(Client)
+            .closeTicket({
+                interaction: interaction,
+                channel: interaction.channel,
+                ticketCreatorId: "755054105713704960",
+                archiveParent: interaction.channel.parent // the channel parent object, can be null
+            })
+    } else if (interaction.customId === "ticketDelete") {
+        new TicketsManager(Client)
+            .deleteTicket({
+                channel: interaction.channel,
+                interaction: interaction
+            })
+    } else if (interaction.customId === "ticketTranscript") {
+        new TicketsManager(Client)
+            .createTranscript({
+                channel: interaction.channel, // the channel to "copy" in the transcript
+                transcriptChannel: interaction.channel, // channel object,
+                interaction: interaction
+            })
+    }
+})
+
+Client.on("messageCreate", async message => {
+    if (message.author.bot) return
+
+    if (message.content.startsWith("!panel_tickets")) {
+
+        const panel = new TicketPanelEmbed(Client)
+            .setChannel(message.channel)
+            .setComponents([
+                new Discord.ActionRowBuilder()
+                    .setComponents([
+                        new Discord.ButtonBuilder()
+                            .setLabel("Open a ticket")
+                            .setEmoji("🎫")
+                            .setStyle(Discord.ButtonStyle.Primary)
+                            .setCustomId("ticketOpen")
+                    ])
+            ])
+            .setEmbeds([
+                new Discord.EmbedBuilder()
+                    .setTitle("Discord-tickets-pro")
+                    .setDescription("Click the button to talk to the staff")
+                    .setFooter({
+                        text: `1.0.3`,
+                        iconURL: Client.user.avatarURL()
+                    })
+            ])
+
+            await panel.send()
+
+            message.reply(":white_check_mark: Panel sended")
+    }
 })

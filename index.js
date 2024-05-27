8bacd27814cc8d4a959649eb385813cbe17b421d
lotus64 <114228798+lotus64yt@users.noreply.github.com>
2024-05-27 22:18:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,169 +1,202 @@
 const Discord = require('discord.js');
+const discordTranscript = require("discord-html-transcripts")
 
-module.exports = class Tickets {
+class TicketPanelEmbed {
   constructor(client) {
     this.client = client;
-    this.guild = null;
     this.channel = null;
-    this.message = null;
-    this.button = {
-      label: null,
-      style: null,
-      emoji: null,
-    };
-    this.staffRole = null
-    this.createdTicket = {
-      message: null
-    }
+    this.actionRows = []
+    this.embeds = []
 
     return this;
   }
 
-  setGuild(guildId) {
-    this.guild = this.client.guilds.cache.get(guildId);
-
+  setEmbeds(embed) {
+    this.embeds = embed
     return this;
   }
 
-  setMessage(message) {
-    this.message = message;
+  setChannel(channel) {
+    this.channel = channel
     return this;
   }
 
-  setTitle(title) {
-    this.title = title;
-    return this;
-  }
+  setComponents(actionRow) {
+    this.actionRows = actionRow
 
-  setThumbnail(thumbnail) {
-    this.thumbnail = thumbnail;
-    return this;
-  }
-
-  setColor(color) {
-    this.color = color;
-    return this;
-  }
-
-  setFooter(footer) {
-    this.footer = footer;
-    return this;
-  }
-
-  setChannel(channelId) {
-    this.channel = this.guild.channels.cache.get(channelId);
-    return this;
-  }
-
-  setButtonLabel(label) {
-    this.button.label = label;
-    return this;
-  }
-
-  setButtonStyle(style) {
-    this.button.style = style;
     return this
   }
 
-  setButtonEmoji(emoji) {
-    this.button.emoji = emoji;
-    return this;
-  }
-
-  setStaffRole(role) {
-    const Role = this.guild.roles.cache.get(role);
-    this.staffRole = Role;
-    return this;
-  }
-
-  setCreatedTicketMessage(message) {
-    this.createdTicket.message = message;
-    return this;
-  }
-
   async send() {
-    if (!this.guild) throw new Error('Guild not set');
     if (!this.channel) throw new Error('Channel not set');
-    if (!this.button.label && !this.button.emoji) throw new Error('Button label or emoji not set')
-    if (!this.button.style) throw new Error('Button style not set');
-    if (!this.createdTicket.message) throw new Error('Created ticket message not set');
+    if (!this.embeds) throw new Error("Embed not set");
+    if (!this.actionRows) throw new Error("Components not set");
 
-    const button = new Discord.ButtonBuilder()
-      .setStyle(this.button.style)
-      .setLabel(this.button.label)
-      .setEmoji(this.button.emoji)
-      .setCustomId('ticketOpen');
+    if (!Array.isArray(this.embeds)) throw new Error("The embed(s) must be in a Array")
+    if (!Array.isArray(this.actionRows)) throw new Error("The component(s) must be in an Array")
 
-    const row = new Discord.ActionRowBuilder().setComponents(button);
-
-    const embed = new Discord.EmbedBuilder()
-      .setTitle(this.title)
-      .setDescription(this.message)
-      .setThumbnail(this.thumbnail)
-      .setColor(this.color ?? "#ADD8E6")
-      .setFooter({ text: this.footer + " ", iconURL: this.client.user.avatarURL() });
-
-    this.client.on('interactionCreate', async interaction => {
-      if (!interaction.isButton()) return;
-
-      if (interaction.guildId !== this.guild.id) throw new Error('This button is not available in this server')
-
-      if (interaction.customId === "ticketClose") {
-        interaction.reply(`Ce ticket sera supprimé dans <t:${Math.floor((Date.now() + 5000) / 1000)}:R> secondes`)
-
-        setTimeout(() => {
-          interaction.channel.delete()
-        }, 5000)
-      }
-
-      if (interaction.customId === 'ticketOpen') {
-        await interaction.guild.members.fetch()
-        const role = await this.guild.roles.fetch(this.staffRole.id);
-
-        if (!role) throw new Error('Role not found');
-
-        interaction.deferReply({ ephemeral: true });
-
-        await interaction.channel.threads.create({
-          name: `ticket-${interaction.user.username}`,
-          type: Discord.ChannelType.PrivateThread,
-          autoArchiveDuration: Discord.ThreadAutoArchiveDuration.OneWeek,
-          reason: `User create a ticket`,
-        }).then(async e => {
-
-
-          e.send({
-            content: `<@${interaction.user.id}>`,
-            embeds: [
-              new Discord.EmbedBuilder()
-                .setColor(this.color ?? "#ADD8E6")
-                .setTitle(this.title)
-                .setDescription(this.createdTicket.message.replaceAll('{member}', `<@${interaction.user.id}>`).replaceAll("{member.id}", interaction.user.id).replaceAll("{member.username}", interaction.user.username))
-                .setFooter({ text: this.footer + " ", iconURL: this.client.user.avatarURL() })
-                .setTimestamp()
-            ],
-            components: [
-              new Discord.ActionRowBuilder()
-                .setComponents([
-                  new Discord.ButtonBuilder()
-                    .setCustomId('ticketClose')
-                    .setStyle(Discord.ButtonStyle.Danger)
-                    .setEmoji('🚮')
-                ])
-            ]
-          })
-
-          const staff = await role.members
-
-          staff.forEach(member => {
-            e.members.add(member.id)
-          })
-
-          interaction.editReply({ content: `:white_check_mark: <#${e.id}>`, ephemeral: true });
-        })
-      }
-    });
-
-    return this.channel.send({ embeds: [embed], components: [row] });
+    return this.channel.send({ embeds: this.embeds, components: this.actionRows });
   }
+}
+
+class TicketsManager {
+  constructor(client) {
+    if (!client) throw new Error("Client not set.")
+
+    this.client = client
+  }
+
+  async createTicket({
+    guild,
+    interaction,
+    embeds,
+    components,
+    accessMembers,
+    parentId = null
+  }) {
+    if (!guild) throw new Error("Guild not set")
+    if (!interaction) throw new Error("Interaction not set")
+    if (!embeds) throw new Error("Embeds not set")
+    if (!components) throw new Error("Components not set")
+    if (!accessMembers) throw new Error("Members who have access to ticket not set")
+
+    if (!Array.isArray(embeds)) throw new Error("Embed(s) must be in an Array")
+    if (!Array.isArray(components)) throw new Error("Component(s) must be in an Array")
+    if (!Array.isArray(accessMembers)) throw new Error("Member(s)'s ids who have access to the ticket must be in an Array")
+
+    if (parentId) {
+      await interaction.deferReply({ ephemeral: true })
+
+      let permissions = []
+
+      accessMembers.forEach(e => permissions.push({
+        id: e,
+        allow: [
+          Discord.PermissionFlagsBits.ViewChannel,
+          Discord.PermissionFlagsBits.SendMessages
+        ]
+      }))
+
+      permissions.push({
+        id: interaction.guild.id,
+        deny: [
+          Discord.PermissionFlagsBits.ViewChannel,
+          Discord.PermissionFlagsBits.SendMessages
+        ]
+      })
+
+      await interaction.guild.channels.create({
+        name: `ticket-${interaction.user.username}`,
+        type: Discord.ChannelType.GuildText,
+        parent: parentId,
+        permissionOverwrites: permissions
+
+      }).then(async e => {
+        await e.send({
+          content: `<@${interaction.user.id}>`,
+          embeds: embeds,
+          components: components
+        })
+
+        interaction.editReply(`:white_check_mark: <#${e.id}>`)
+      })
+
+
+    } else {
+      await interaction.deferReply({ ephemeral: true })
+
+      await interaction.channel.threads.create({
+        name: `ticket-${interaction.user.username}`,
+        type: Discord.ChannelType.PrivateThread,
+        autoArchiveDuration: Discord.ThreadAutoArchiveDuration.OneWeek,
+        reason: `User create a ticket`,
+      }).then(async e => {
+        await e.send({
+          content: `<@${interaction.user.id}>`,
+          embeds: embeds,
+          components: components
+        })
+
+        interaction.editReply(`:white_check_mark: <#${e.id}>`)
+
+        accessMembers.forEach(m => {
+          e.members.add(m.id)
+        })
+      })
+    }
+  }
+
+  async createTranscript({
+    channel,
+    transcriptChannel,
+    interaction
+  }) {
+    if (!channel) throw new Error("Channel to transcript not set")
+    if (!transcriptChannel) throw new Error("Channel to send transcript not set")
+    if (!interaction) throw new Error("Interaction not set")
+
+    interaction.reply({content: ":white_check_mark:", ephemeral: true})
+
+    const attachment = await discordTranscript.createTranscript(channel)
+    transcriptChannel.send({
+      files: [attachment]
+    })
+  }
+
+  async closeTicket({
+    channel,
+    interaction,
+    ticketCreatorId,
+    archiveParent = null,
+  }) {
+    if (!channel) throw new Error("Channel not set")
+    if (!ticketCreatorId) throw new Error("Ticket Creator not set")
+    if (channel.type === Discord.ChannelType.PrivateThread) throw new Error("Cannot close a thread ticket, try deleteTicket() method.")
+
+    if (archiveParent) {
+      await interaction.reply({ content: ":white_check_mark:", ephemeral: true })
+
+      await channel.setParent(archiveParent)
+
+      await channel.permissionOverwrites.edit(ticketCreatorId, {
+        ViewChannel: false
+      })
+
+      await channel.permissionOverwrites.edit(channel.guild.roles.everyone, {
+        ViewChannel: false
+      })
+    } else {
+      await interaction.reply({ content: ":white_check_mark:", ephemeral: true })
+
+      await channel.permissionOverwrites.edit(ticketCreatorId, {
+        ViewChannel: false
+      })
+
+      await channel.permissionOverwrites.edit(channel.guild.roles.everyone, {
+        ViewChannel: false
+      })
+
+
+    }
+    await channel.edit({
+      name: channel.name.replace("ticket-", "closed-")
+    })
+  }
+
+  async deleteTicket({
+    channel,
+    interaction
+  }) {
+    if (!channel) throw new Error("Channel not set.")
+    if (!interaction) throw new Error("Interaction not set")
+
+    await interaction.deferReply({ ephemeral: true })
+
+    await channel.delete()
+  }
+}
+
+module.exports = {
+  TicketPanelEmbed,
+  TicketsManager
 }
